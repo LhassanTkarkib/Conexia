de25572ca1f8dfa75991e86222dd33baaf625455
rachid-boutahir <rachid.boutahir.com@gmail.com>
2024-02-26 23:17:10 +0100

    main Friend modification

--- FriendMicroService/src/main/java/com/simplon/Friend.java
+++ FriendMicroService/src/main/java/com/simplon/Friend.java
@@ -1,16 +1,17 @@
 package com.simplon;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
- *
  */
 @SpringBootApplication
-public class Friend
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Friend {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        SpringApplication.run(Friend.class, args);
     }
 }
