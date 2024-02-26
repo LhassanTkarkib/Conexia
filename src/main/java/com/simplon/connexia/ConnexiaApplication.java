4a38918bdf1406ab0dae05c840b607e582ad19da
rachid-boutahir <rachid.boutahir.com@gmail.com>
2024-02-26 12:22:00 +0100

    initialisation de projet microservices

---
+++ src/main/java/com/simplon/connexia/ConnexiaApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.connexia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnexiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnexiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplon/connexia/ConnexiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.connexia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnexiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
