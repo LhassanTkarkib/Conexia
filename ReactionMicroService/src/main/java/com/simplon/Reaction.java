7901210a7cc4cb6b74ae9a3a1cea25b0de7c420e
ayoub <AITSIAHMAD.AYOUB@ofppt-edu.ma>
2024-03-01 11:21:30 +0100

    add the notification clients

--- ReactionMicroService/src/main/java/com/simplon/Reaction.java
+++ ReactionMicroService/src/main/java/com/simplon/Reaction.java
@@ -9,8 +9,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-@EnableFeignClients
 @EnableDiscoveryClient
+@EnableFeignClients(
+        basePackages = "com.simplon"
+)
 public class Reaction {
     public static void main(String[] args) {
         SpringApplication.run(Reaction.class, args);
