23f5168e469e84481738df7242b6022dc0d678cc
rachid-boutahir <rachid.boutahir.com@gmail.com>
2024-03-01 12:16:13 +0100

    mise a jour de l'interface de microservice feign-client

--- feign-clients/src/main/java/com/simplon/friendship/FriendshipClient.java
+++ feign-clients/src/main/java/com/simplon/friendship/FriendshipClient.java
@@ -1,6 +1,7 @@
 package com.simplon.friendship;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @FeignClient("FRIEND")
 public interface FriendshipClient {
+
     @GetMapping("/api/v1/friend-ships/requested-accepted-rejected/all")
     ResponseEntity<List<FriendShipDto>> getAllFriendRequestedAcceptedRejected();
 
@@ -24,12 +26,19 @@ public interface FriendshipClient {
     ResponseEntity<FriendShipDto> addFriendShip(@RequestBody FriendShipDto friendShipDto);
 
     @PutMapping("/api/v1/friend-ships/update-friend/{friendId}")
-    ResponseEntity<FriendShipDto> updateFriendShip();
+    ResponseEntity<FriendShipDto> updateFriendShip(
+            @PathVariable("friendId") long friendId,
+            @RequestBody FriendShipDto friendShipDto) throws ChangeSetPersister.NotFoundException;
 
     @DeleteMapping("/api/v1/friend-ships/delete-friend")
-    ResponseEntity<String> deleteFriendByUserIdAndFriendId();
+    ResponseEntity<String> deleteFriendByUserIdAndFriendId(
+            @RequestParam("userId") Long userId,
+            @RequestParam("friendId") Long friendId) throws ChangeSetPersister.NotFoundException;
 
     @GetMapping("/api/v1/friend-ships/search-Specific-friend")
-    ResponseEntity<FriendShipDto> searchSpecificFriend();
+    ResponseEntity<FriendShipDto> searchSpecificFriend(
+            @RequestParam("userId") long userId,
+            @RequestParam("friendId") long friendId,
+            @RequestParam("status") StatusFriendEnum status) throws ChangeSetPersister.NotFoundException;
 
 }
