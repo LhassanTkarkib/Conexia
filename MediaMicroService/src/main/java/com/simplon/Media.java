d183b65156cb152c67e9615f10c978ee97588d81
ayoub <AITSIAHMAD.AYOUB@ofppt-edu.ma>
2024-03-01 11:57:13 +0100

    all good about the feign clients module

--- MediaMicroService/src/main/java/com/simplon/Media.java
+++ MediaMicroService/src/main/java/com/simplon/Media.java
@@ -14,7 +14,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *
  */
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(
+        basePackages = "com.simplon"
+)
 @EnableConfigurationProperties({
         FileStorageProperties.class
 })
