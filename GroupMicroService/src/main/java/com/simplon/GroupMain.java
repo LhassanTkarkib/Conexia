f1aee203b7b787599a9069f0842dfe391f6e229f
ayoub <AITSIAHMAD.AYOUB@ofppt-edu.ma>
2024-03-01 10:05:01 +0100

    fix merging problems

--- GroupMicroService/src/main/java/com/simplon/GroupMain.java
+++ GroupMicroService/src/main/java/com/simplon/GroupMain.java
@@ -10,7 +10,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableWebMvc
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.simplon"})
-@EnableDiscoveryClient
 public class GroupMain {
     public static void main(String[] args) {
         SpringApplication.run(GroupMain.class, args);
