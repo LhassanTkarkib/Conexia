fe625c73f2b5648832df35918ffbcc27c0cf4b84
Chaimaa mahy <chaim.yaham17@gmail.com>
2024-03-01 09:56:36 +0100

    MC-13 modify app.prop

--- ReactionMicroService/src/main/java/com/simplon/Reaction.java
+++ ReactionMicroService/src/main/java/com/simplon/Reaction.java
@@ -2,6 +2,7 @@ package com.simplon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableFeignClients
 public class Reaction {
     public static void main(String[] args) {
         SpringApplication.run(Reaction.class, args);
