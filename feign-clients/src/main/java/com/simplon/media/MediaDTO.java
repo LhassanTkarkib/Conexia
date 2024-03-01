109966a234e01f8bb9051e1d2bf730849c864db1
Talaini <10530576+talainimohammed@users.noreply.github.com>
2024-03-01 10:55:29 +0100

    add media and post Feign interfaces

---
+++ feign-clients/src/main/java/com/simplon/media/MediaDTO.java
@@ -0,0 +1,12 @@
+package com.simplon.media;
+
+
+
+public class MediaDTO {
+    private long fileId;
+    private String name;
+    private String fileUrl;
+    private TypeFile typeFile;
+    private String dateCreation;
+    private long postId;
+}
